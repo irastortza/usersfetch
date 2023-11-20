a85ebd7500e32a142abaa1c33959191b29a0818d
Xabier Irastorza <76776384+irastortza@users.noreply.github.com>
2023-11-20 14:20:43 +0000

    express moduan jarri

--- ariketa3/app.js
+++ ariketa3/app.js
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
-
+app.use("/style/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
